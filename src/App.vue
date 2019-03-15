e206345a3e3c98e6e2b551a18f6f376d3a4d5e3e
pigeon00 <xue.meng.happy@163.com>
2019-03-15 10:08:57 +0800

    master:init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from "axios";
+  Vue.prototype.$axios = axios;
+
+
+
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
